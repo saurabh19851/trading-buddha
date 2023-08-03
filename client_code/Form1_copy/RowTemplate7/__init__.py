9ba8f0338eff7cca973c9795f3408e4d72a37fc8
saurabh198511@gmail.com <saurabh198511@gmail.com>
2023-08-03 23:00:44 +0000

    Edited Form1_copy.RowTemplate7

--- client_code/Form1_copy/RowTemplate7/__init__.py
+++ client_code/Form1_copy/RowTemplate7/__init__.py
@@ -4,10 +4,14 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ... import Globals
 
 class RowTemplate7(RowTemplate7Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    ticker=Globals.ticker
+    dates,title,text,url,images=anvil.server.call('news',)
+    self.news_title_link.tag
 
     # Any code you write here will run before the form opens.
