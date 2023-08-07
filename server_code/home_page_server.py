e3b8b0cec9195fae78a3ef93b117adc4777111e3
saurabh198511@gmail.com <saurabh198511@gmail.com>
2023-08-07 04:08:33 +0000

    Created Home.RowTemplate5 and home_page_server; Edited Home

---
+++ server_code/home_page_server.py
@@ -0,0 +1,37 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+import datetime
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def api_key():
+  fmp_api_key='622f44c679bbfc88f813b6d43f217749'
+  return fmp_api_key
+
+
+@anvil.server.callable
+def economic_calendar():
+  start_date=str(datetime.timedelta(days=10))
+  end_date=str(datetime.timedelta(days=-10))
+  param = {'from':start_date,'to':end_date,'apikey': api_key()} 
+  req=requests.get('https://financialmodelingprep.com/api/v3/economic_calendar?', param)
+  data=req.json()
+  data_US=[]
+  for x in data:
+    if x['country']=='US':
+      data_US.append(x)
+  return data_US
