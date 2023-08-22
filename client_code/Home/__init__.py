4c3b18cbeb3d08ac3c5807a9f299ac50c1c8526e
saurabh198511@gmail.com <saurabh198511@gmail.com>
2023-08-22 03:42:34 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -25,9 +25,15 @@ class Home(HomeTemplate):
     self.vix.figure=anvil.server.call('indices_graph','^IXIC','VIX')
     self.news_panel.items=anvil.server.call('general_stock_news')
     self.rich_text_1.background='rgba(255,99,71,0.5)'
-    major_indicies=['^GSPC','^IXIC','^DJI','^VIX','RUA','CLUSD','GCUSD']
+    major_indicies=['^GSPC','^IXIC','^DJI','^VIX','^RUA','CLUSD','GCUSD']
     major_indices_data=anvil.server.call('indices_day_change',major_indicies)
-    
+    self.dr_sp500.content=str("S&P500: "+f"{major_indices_data[0]['1D']:.2f}%")
+    self.dr_nasdaq.content=str("NASDAQ: "+f"{major_indices_data[1]['1D']:.2f}%")
+    self.dr_dji.content=str("Dow Jones: "+f"{major_indices_data[2]['1D']:.2f}%")
+    self.dr_vix.content=str("VIX: "+f"{major_indices_data[3]['1D']:.2f}%")
+    self.dr_russell3000.content=str("Russell 3000: "+f"{major_indices_data[4]['1D']:.2f}%")
+    self.dr_oil.content=str("Oil: "+f"{major_indices_data[5]['1D']:.2f}%")
+    self.dr_gold.content=str("Gold: "+f"{major_indices_data[6]['1D']:.2f}%")
     
 
   def form_show(self, **event_args):
