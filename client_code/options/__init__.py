7562da54cb942e3a8e6a69831708181354cf308b
saurabh198511@gmail.com <saurabh198511@gmail.com>
2023-08-28 15:13:08 +0000

    Created options.RowTemplate3 and options

---
+++ client_code/options/__init__.py
@@ -0,0 +1,33 @@
+from ._anvil_designer import optionsTemplate
+from anvil import *
+import plotly.graph_objects as go
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+from .. import Globals
+
+
+class options(optionsTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    # Any code you write here will run before the form opens.
+    #Code for getting stock basic information
+    ticker=Globals.ticker
+    
+
+
+
+
+  def form_refreshing_data_bindings(self, **event_args):
+    """This method is called when refreshing_data_bindings is called"""
+    pass
+
+  def form_show(self, **event_args):
+    pass
+
+ 
+  def autocomplete_1_change(self, **event_args):
+    self.autocomplete_1.suggestions=['AAPL','AAPK','AAPT','AAPP','AAPB','AAPC']
+
