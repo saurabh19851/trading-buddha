24ce70ebc4927e5808de8024e3a0a8970976dcfb
saurabh198511@gmail.com <saurabh198511@gmail.com>
2023-11-05 10:06:39 +0000

    Edited theme/parameters.yaml and Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -71,58 +71,44 @@ class Home(HomeTemplate):
 
     self.dr_sp500.text=str("S&P 500: "+f"{major_indices_data[0]['1D']:.2f}%")
     if major_indices_data[0]['1D']>0:
-      self.dr_sp500.background='rgba(124, 252, 0,0.2)'
       self.dr_sp500.foreground='rgba(0, 128, 0,1)'
     else:
-      self.dr_sp500.background='rgba(255, 87, 51,0.2)'
       self.dr_sp500.foreground='rgba(255, 87, 51,1)'
       
-    self.dr_nasdaq.content=str("NASDAQ: "+f"{major_indices_data[1]['1D']:.2f}%")
+    self.dr_nasdaq.text=str("NASDAQ: "+f"{major_indices_data[1]['1D']:.2f}%")
     if major_indices_data[1]['1D']>0:
-      self.dr_nasdaq.background='rgba(124, 252, 0,0.2)'
       self.dr_nasdaq.foreground='rgba(0, 128, 0,1)'
     else:
-      self.dr_nasdaq.background='rgba(255, 87, 51,0.2)'
       self.dr_nasdaq.foreground='rgba(255, 87, 51,1)'
       
-    self.dr_dji.content=str("Dow Jones: "+f"{major_indices_data[2]['1D']:.2f}%")
+    self.dr_dji.text=str("Dow Jones: "+f"{major_indices_data[2]['1D']:.2f}%")
     if major_indices_data[2]['1D']>0:
-      self.dr_dji.background='rgba(124, 252, 0,0.2)'
       self.dr_dji.foreground='rgba(0, 128, 0,1)'
     else:
-      self.dr_dji.background='rgba(255, 87, 51,0.2)'
       self.dr_dji.foreground='rgba(255, 87, 51,1)'
       
-    self.dr_vix.content=str("VIX: "+f"{major_indices_data[3]['1D']:.2f}%")
+    self.dr_vix.text=str("VIX: "+f"{major_indices_data[3]['1D']:.2f}%")
     if major_indices_data[3]['1D']>0:
-      self.dr_vix.background='rgba(124, 252, 0,0.2)'
       self.dr_vix.foreground='rgba(0, 128, 0,1)'
     else:
-      self.dr_vix.background='rgba(255, 87, 51,0.2)'
       self.dr_vix.foreground='rgba(255, 87, 51,1)'
       
-    self.dr_russell3000.content=str("Russell 3000: "+f"{major_indices_data[4]['1D']:.2f}%")
+    self.dr_russell3000.text=str("Russell 3000: "+f"{major_indices_data[4]['1D']:.2f}%")
     if major_indices_data[4]['1D']>0:
-      self.dr_russell3000.background='rgba(124, 252, 0,0.2)'
       self.dr_russell3000.foreground='rgba(0, 128, 0,1)'
     else:
-      self.dr_russell3000.background='rgba(255, 87, 51,0.2)'
       self.dr_russell3000.foreground='rgba(255, 87, 51,1)'
       
-    self.dr_oil.content=str("Oil: "+f"{major_indices_data[5]['1D']:.2f}%")
+    self.dr_oil.text=str("Oil: "+f"{major_indices_data[5]['1D']:.2f}%")
     if major_indices_data[5]['1D']>0:
-      self.dr_oil.background='rgba(124, 252, 0,0.2)'
       self.dr_oil.foreground='rgba(0, 128, 0,1)'
     else:
-      self.dr_oil.background='rgba(255, 87, 51,0.2)'
       self.dr_oil.foreground='rgba(255, 87, 51,1)'
       
-    self.dr_gold.content=str("Gold: "+f"{major_indices_data[6]['1D']:.2f}%")
+    self.dr_gold.text=str("Gold: "+f"{major_indices_data[6]['1D']:.2f}%")
     if major_indices_data[6]['1D']>0:
-      self.dr_gold.background='rgba(124, 252, 0,0.2)'
       self.dr_gold.foreground='rgba(0, 128, 0,1)'
     else:
-      self.dr_gold.background='rgba(255, 87, 51,0.2)'
       self.dr_gold.foreground='rgba(255, 87, 51,1)'
 
     
