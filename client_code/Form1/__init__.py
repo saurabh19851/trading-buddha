1d6a699fc290779df1d655fc110c99e042a5deca
saurabh198511@gmail.com <saurabh198511@gmail.com>
2024-03-30 16:38:30 +0000

    Created Form1; Edited settings

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,14 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+import anvil.server
+from anvil.js.window import jQuery
+from anvil.js import get_dom_node
+
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    iframe = jQuery("<iframe width='100%' height='100%'>").attr("src","https://fred.stlouisfed.org/graph/graph-landing.php?g=1jhCp")
+    iframe.appendTo(get_dom_node(self.xy_panel_1))
+    # Any code you write here will run before the form opens.
